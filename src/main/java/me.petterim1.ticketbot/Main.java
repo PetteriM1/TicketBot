f8e6fbaa24916bb2b0ca94b33890a69409fb8adf
Petteri <26197131+PetteriM1@users.noreply.github.com>
2022-07-24 22:09:21 +0300

    Load empty config from jar if no config exists

--- src/main/java/me.petterim1.ticketbot/Main.java
+++ src/main/java/me.petterim1.ticketbot/Main.java
@@ -4,17 +4,17 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.*;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.components.Button;
 import net.dv8tion.jda.api.interactions.components.ButtonStyle;
 
 import javax.security.auth.login.LoginException;
 import java.awt.*;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Properties;
 
-public class Main extends ListenerAdapter {
+public class Main {
 
     static JDA JDA;
     static Properties CONFIG = new Properties();
@@ -23,9 +23,7 @@ public class Main extends ListenerAdapter {
         log("Discord ticket bot made by PetteriM1");
         log("---------------------");
         log("Loading config...");
-        FileInputStream propsInput = new FileInputStream("config.txt");
-        CONFIG.load(propsInput);
-        propsInput.close();
+        loadConfig();
         log("Logging in to Discord...");
         JDA = JDABuilder.createDefault(CONFIG.getProperty("bot_token")).build();
         log("Waiting JDA to load...");
@@ -37,6 +35,16 @@ public class Main extends ListenerAdapter {
         log("Preparing ticket channel...");
         prepareChannel();
         log("The bot is online!");
+    }
+
+    private static void loadConfig() throws IOException {
+        if (!new File("config.txt").exists()) {
+            log("No config.txt found, creating an empty config...");
+            exportDefaultConfig();
+        }
+        FileInputStream propsInput = new FileInputStream("config.txt");
+        CONFIG.load(propsInput);
+        propsInput.close();
     }
 
     private static void prepareChannel() {
@@ -65,5 +73,29 @@ public class Main extends ListenerAdapter {
 
     static void log(String text) {
         System.out.println(text);
+    }
+
+    private static void exportDefaultConfig() throws IOException {
+        InputStream stream = null;
+        OutputStream resStreamOut = null;
+        try {
+            stream = Main.class.getClassLoader().getResourceAsStream("config.txt.empty");
+            if (stream == null) {
+                throw new RuntimeException("Cannot get 'config.txt.empty' from the jar file!");
+            }
+            resStreamOut = Files.newOutputStream(Paths.get(new File("config.txt").toURI()));
+            byte[] buffer = new byte[4096];
+            int readBytes;
+            while ((readBytes = stream.read(buffer)) > 0) {
+                resStreamOut.write(buffer, 0, readBytes);
+            }
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+            if (resStreamOut != null) {
+                resStreamOut.close();
+            }
+        }
     }
 }
